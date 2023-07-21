84173a026596abea25c7446abaf273c27a6efbc1
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-07-20 22:53:04 -0500

    hello wordl

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# it's a comment 
+print("hello world")
+
+#data type query
+print(type("I'm a string data")) #type str
+print(type(5)) #type int
+print(type(1.5)) #type float
+print(type(3 + 1j)) #type complex
+print(type(True)) #type bool
+print(type(print("string"))) #type none type
