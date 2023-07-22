7f65ebdef76d0d6dd63b37ff1baefb19bc977978
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-07-22 15:55:21 -0500

    python conditionals

---
+++ 08_conditional.py
@@ -0,0 +1,25 @@
+### Conditionals
+
+# if
+
+if 5 > 4:
+    print("this is max")
+
+condition = 5 * 2;
+
+if condition == 10:
+    print(True)
+elif condition != 10:
+    print("it's different")
+else:
+    print("another number")
+
+print("continue execute?")
+
+my_string = ""
+
+if not my_string:
+    print("it's empty")
+
+if my_string == "My string":
+    print("this string are same")
