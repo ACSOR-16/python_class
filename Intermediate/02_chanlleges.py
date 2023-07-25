fca3f2addc430a7d47513f845bfefc4f0509dc47
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2023-07-24 23:37:43 -0500

    python challenges - fizzbuzz

---
+++ Intermediate/02_chanlleges.py
@@ -0,0 +1,19 @@
+### Challenges ###
+
+"""
+"FIZZ BUZZ”:
+"""
+def fizz_buzz():
+    for element in range(1, 101):
+        if element % 3 == 0 and element % 5 == 0:
+            print("FizzBuzz")
+        elif element % 3 == 0:
+            print("Fizz")
+        elif element % 5 == 0:
+            print("Buzz")
+        else:
+            print(element)
+
+#fizz_buzz()
+
+print(False or True)
